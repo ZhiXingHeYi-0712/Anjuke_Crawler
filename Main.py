0f8656e46dbd5cae8150485de1982d790aa2d3e1
ZhiXingHeYi-0712 <Lihao.0712@outlook.com>
2021-03-20 00:28:33 +0800

    init 2

--- Main.py
+++ Main.py
@@ -5,9 +5,8 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 c = Crawler('https://shenzhen.anjuke.com/sale/p{page}/?from=esf_list', city='深圳')
-p = c.getPageRangeResult(1, 50)
+p = c.getPageRangeResult(51, 100)
 formatter = DataFormatter(p)
 gdf = formatter.getMeanPriceGeoDataFrame()
 gdf.to_file('shenzhen_house.shp', encoding='utf-8')
-
 
